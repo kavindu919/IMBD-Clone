a98b6ec90a875b2038e50a3ca5c20d4c8d050d88
kavindu919 <jayakodyk2@gmail.com>
2024-05-14 10:34:01 +0530

    add about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className=" max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className=" text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        The IMDb clone app's About page provides users with comprehensive
+        insights into the platform's mission, vision, and purpose. Serving as a
+        hub for film and television enthusiasts, our About page delves into the
+        app's commitment to delivering accurate and up-to-date information about
+        movies, TV shows, actors, and filmmakers. We aim to cultivate a
+        community where users can explore, discover, and engage with their
+        favorite content while also discovering new cinematic gems.
+        Additionally, the About page sheds light on the technology and
+        methodologies employed to ensure data accuracy and user satisfaction,
+        highlighting our dedication to providing a seamless and enriching user
+        experience. Through transparent communication and a passion for cinema,
+        our About page fosters a deeper understanding of the IMDb clone app's
+        role in the entertainment landscape and invites users to join us on this
+        cinematic journey.
+      </p>
+    </div>
+  );
+}
